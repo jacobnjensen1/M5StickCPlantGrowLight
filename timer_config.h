a736581c4e957a0dc9ac48f347aadda2c867c965
Jacob Jensen <jnjensen88@gmail.com>
2020-06-02 21:23:56 -0600

    fix schedule

--- timer_config.h
+++ timer_config.h
@@ -1,5 +1,5 @@
 //TimeSpecs use 24hr time
-TimeSpec on_times[] =  {TimeSpec(4, 0, 0)}; //Add TimeSpecs as desired
-TimeSpec off_times[] = {TimeSpec(20, 0, 0)};
+TimeSpec on_times[] =  {TimeSpec(8, 0, 0)}; //Add TimeSpecs as desired
+TimeSpec off_times[] = {TimeSpec(16, 0, 0)};
 //array format: {TimeSpec(hr, min, sec), TimeSpec(hr, min,sec)}
 //values of 8 and 9 cannot be inserted as 08 or 09 (leading 0 specifies base 8)
